d6d8ef0bd88e9a501d89667a808dfc83922d4ca0
Rayan fernandes <37145078+Rayanfer32@users.noreply.github.com>
2020-11-29 20:35:44 +0530

    asset error

--- app/src/main/java/com/example/recyclerviewexp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewexp/MainActivity.kt
@@ -1,53 +1,60 @@
 package com.example.recyclerviewexp
 
-import Json4Kotlin_Base
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.nfc.Tag
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.ListAdapter
-import android.widget.ListView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONArray
+import java.io.File
 import java.io.IOException
-import java.io.InputStream
-import java.security.AccessController.getContext
 import kotlin.random.Random
 
+
 class MainActivity : AppCompatActivity() ,ItemAdapter.OnItemClickListener{
-    private var exampleList = loadJsonItems(applicationContext)
-    private val adapter = ItemAdapter(exampleList,this)
+     private lateinit var exampleList: ArrayList<ExampleItem>
+     private lateinit var adapter: ItemAdapter
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        exampleList = loadJsonItems()
+        adapter = ItemAdapter(exampleList, this)
         recycler_view.adapter = adapter
-        recycler_view.layoutManager = GridLayoutManager(this,3)
+        recycler_view.layoutManager = GridLayoutManager(this, 3)
         recycler_view.setHasFixedSize(true)
     }
 
-    private fun loadJsonItems(context:Context): ArrayList<ExampleItem> {
+    private fun loadJsonItems(): ArrayList<ExampleItem> {
         val arrlist = ArrayList<ExampleItem>()
-        var json: String = ""
+//        var json: String = ""
         try {
-            val inputStream: InputStream = context.assets.open("products.json") // your json value here
-            json = inputStream.bufferedReader().use{ it.readText() }
-//        val topic = Gson().fromJson(json, Json4Kotlin_Base::class.java)
-
+//            val inputStream: InputStream = assets.open("products.json") // your json value here
+            val json = application.assets.open("products.json").bufferedReader().use{
+                it.readText()
+            }
+            //val topic = Gson().fromJson(json, Json4Kotlin_Base::class.java)
             var jsonarr = JSONArray(json)
             println(jsonarr.length())
             for (i in 0..jsonarr.length() - 1) {
                 var jsonobj = jsonarr.getJSONObject(i)
+                val itemName:String = jsonobj.getString("name")
+                var drawable:Int = R.drawable.fries
+
+
+//                drawable = getImageIdFromAsset(itemName)
+                Log.i("INFO", filePath(itemName))
+
                 arrlist += ExampleItem(
-                    R.drawable.ic_launcher_foreground,
-                    jsonobj.getString("brand"),
-                    jsonobj.getString("name")
+                        drawable,//Pass only int id
+                        itemName,
+                        jsonobj.getString("brand")
                 )
             }
         }
@@ -56,6 +63,33 @@ class MainActivity : AppCompatActivity() ,ItemAdapter.OnItemClickListener{
             return arrlist
         }
         return arrlist
+    }
+
+    fun filePath(image: String, _context: Context): String? {
+        val file = File(_context.getCacheDir() + File.separator.toString() + image)
+        return if (file.exists()) file.getPath() else null
+    }
+
+    private fun getImageIdFromAsset(filename: String): Int {
+        TODO("Find the icon asset from its itemname and return its id " +
+                "Split the itemName into aliases using seperator for | and check if\" +\n" +
+                   " the icon exists in the svg asset folder ")
+
+        val itemAliases = filename.split("|",
+                ignoreCase =false,
+                limit = 0)
+        for alias in itemAliases{
+
+        }
+//                try {
+//                    if(itemName.contains(itemName, ignoreCase = true)){
+//                        drawable = resources.getIdentifier("svg/$itemName", "drawable","com.example.recyclerviewexp")
+//                    }
+//                }
+//                catch(e:Exception){
+//                    //fallback icon
+//                    drawable = R.drawable.fries
+//                }
     }
 
     private fun generateDummyList(size: Int): ArrayList<ExampleItem>{
@@ -67,7 +101,7 @@ class MainActivity : AppCompatActivity() ,ItemAdapter.OnItemClickListener{
                 1 -> R.drawable.ic_launcher_foreground
                 else -> R.drawable.ic_launcher_background
             }
-            val item = ExampleItem(drawable,text1 = "Item $i",text2 = "This is an item $i")
+            val item = ExampleItem(drawable, text1 = "Item $i", text2 = "This is an item $i")
             list += item
         }
 
@@ -89,11 +123,11 @@ class MainActivity : AppCompatActivity() ,ItemAdapter.OnItemClickListener{
     fun insertItem(view: View) {
         val index:Int = Random.nextInt(8)
         val newItem = ExampleItem(
-            R.drawable.ic_launcher_foreground,
-            text1 = "NewItem $index",
-            text2 = "Newitem $index was added!"
+                R.drawable.ic_launcher_foreground,
+                text1 = "NewItem $index",
+                text2 = "Newitem $index was added!"
         )
-        exampleList.add(index,newItem)
+        exampleList.add(index, newItem)
         adapter.notifyItemInserted(index)
     }
 
